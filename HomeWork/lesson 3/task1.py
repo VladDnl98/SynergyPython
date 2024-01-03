3122e1183cf6becab5d748074531a3216c5dac92
Vladislav Danilchuk <vladislavdanilchuk@yandex.ru>
2024-01-03 14:29:37 +0300

    Выполнено домашнее задание по уроку 3

---
+++ HomeWork/lesson 3/task1.py
@@ -0,0 +1,6 @@
+typePet = input("Введите вид питомца: ")
+namePet = input("Введите кличку питомца: ")
+agePet = input("Введите возраст питомца: ")
+
+
+print("Это", typePet, "по кличке", namePet,"и его возраст", agePet, sep=" ")
