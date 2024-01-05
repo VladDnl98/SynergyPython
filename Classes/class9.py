6eda286ed604eea5b776d761b0b932bcdae6e2fb
Vladislav Danilchuk <vladislavdanilchuk@yandex.ru>
2024-01-05 13:50:29 +0300

    Выполнено домашнее задание по уроку 9

---
+++ Classes/class9.py
@@ -0,0 +1,47 @@
+# tmp = {1, 2, 4, 3, 5, 9, 0}
+# for el in tmp:
+#     print(el)
+
+
+
+# s1 = frozenset()
+# s2 = s1
+# s2.add(7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# c1 = int(input())
+# cl1 = []
+
+# for i in range(c1):
+#     name = input()
+#     cl1.append(name)
+
+# uc1 = set(cl1)
+# print(uc1)
+
+# c2 = int(input())
+# cl2 = []
+# for i in range(c2):
+#     name = input()
+#     cl2.append(name)
+
+# uc2 = set(cl2)
+# print(uc2)
+
+# print(uc1.union(uc2))
