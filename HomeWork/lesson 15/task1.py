90f3706a8ffcea726c3e3fa64ffacb2b17eb7f96
Vladislav Danilchuk <vladislavdanilchuk@yandex.ru>
2024-01-10 13:43:27 +0300

    Исправлено задание 1 по уроку 15

--- HomeWork/lesson 15/task1.py
+++ HomeWork/lesson 15/task1.py
@@ -4,13 +4,10 @@ class Transport:
         self.max_speed = max_speed
         self.mileage = mileage
 
-class Bus(Transport):
-    def __init__(self, name, max_speed, mileage):
-        super().__init__(name, max_speed, mileage)
-    
     def __str__(self):
-        return "Название автомобиля: "+self.name+" Скорость: "+str(self.max_speed)+" Пробег: "+str(self.mileage)
-    
-bus=Bus("Renault Logan", 180, 12)
+        return "Название автомобиля: " + self.name + " Скорость: " + str(self.max_speed) + " Пробег: " + str(self.mileage)
+
+
+bus = Transport("Renault Logan", 180, 12)
 
 print(bus)
