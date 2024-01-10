6065d1e673a6df999b9fcb18ee97e184dd481393
Vladislav Danilchuk <vladislavdanilchuk@yandex.ru>
2024-01-10 13:16:00 +0300

    Исправлено задание 1 по модулю 11

--- HomeWork/lesson 11/task1.py
+++ HomeWork/lesson 11/task1.py
@@ -1,9 +1,19 @@
-def fac(n):
-    if n == 0:
+def factorial(n):
+    if n == 0 or n == 1:
         return 1
-    return fac(n-1) * n
+    else:
+        return n * factorial(n - 1)
 
-y=int(input())
+def get_factorial_list(number):
+    fact = factorial(number)
+    result = [factorial(i) for i in range(1, min(number+5, number+1))]
+    result.reverse()
+    return result
 
-for i in range(y,0,-1):
-    print(fac(i))
+# Пример использования
+input_number = int(input("Введите число: "))
+result_factorial = factorial(input_number)
+result_list = get_factorial_list(result_factorial)
+
+print("Факториал числа", input_number, ":", result_factorial)
+print("Результирующий список факториалов:", result_list)
